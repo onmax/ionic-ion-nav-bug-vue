e72f276ffd2d2854ec736f1da4d2b2f9cda78883
0x467 <0x467@pm.me>
2023-05-09 12:47:53 +0300

    initial commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-nav :root="PageOne"></ion-nav>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonNav } from '@ionic/vue';
+import PageOne from "@/components/PageOne.vue";
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
